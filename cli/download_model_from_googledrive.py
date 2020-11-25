ba055202aed6e1e726c1779ecd0a5633d618828d
arjangroen <arjanmartengroen@gmail.com>
2020-11-25 20:04:59 +0100

    :alembic: Google Drive IO experiment

---
+++ cli/download_model_from_googledrive.py
@@ -0,0 +1,6 @@
+from utils.googledrive_requests import download_from_googledrive
+
+file_id = "1QSRCuV0PBi20fIm1yblB4CWH5p_V6EVo"
+filepath = "../data/models/resnet_model"
+
+download_from_googledrive(file_id, filepath)
