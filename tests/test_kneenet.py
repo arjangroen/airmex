9d0aabecbb5757614cc71eb2817f68cab1f0a8c9
rikkraanvantage <rik.kraan@vantage-ai.com>
2020-12-02 09:28:28 +0100

    made pytest ignore warnings raised by using old torchvision module

--- tests/test_kneenet.py
+++ tests/test_kneenet.py
@@ -1,6 +1,8 @@
 from utils.build_model import rebuild_kneenet
+import pytest
 import torchvision
 
+# @pytest.mark.filterwarnings('ignore::UserWarning')
 def test_rebuild_model():
     pretrained_model = rebuild_kneenet()
     assert type(pretrained_model) == torchvision.models.densenet.DenseNet
--- utils/build_model.py
+++ utils/build_model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 import os
+from pathlib import Path
 
 def rebuild_kneenet():
     # Initialize densenet architecture
@@ -18,8 +19,11 @@ def rebuild_kneenet():
     for param in pretrained_model.classifier.parameters():
         param.requires_grad = False
 
+    # Get model's location
+    root_dir = Path(__file__).parent.parent
+    model_dir = os.path.join(root_dir, 'data/models/KneeNet')
     # Load the model's trained weights
-    pretrained_model.load_state_dict(torch.load(os.path.abspath('data/models/KneeNet'),
+    pretrained_model.load_state_dict(torch.load(os.path.abspath(model_dir),
                                                 map_location=lambda storage, loc: storage))
     pretrained_model.train(False)
     pretrained_model = pretrained_model.float()
