8ed875db8c9947788102bc5deeb52b87b7fd9ee0
arjangroen <arjanmartengroen@gmail.com>
2020-12-22 19:01:11 +0100

    :sparkles: Captum DeepLift Poc

--- cli/evaluate_mendeley_dataset.py
+++ cli/evaluate_mendeley_dataset.py
@@ -10,30 +10,53 @@ filepaths = [filepath_base + str(kl) for kl in range(5)]
 
 predictions = []
 actuals = []
-target_names = ["KL 0","KL 1", "KL 2", "KL 3", "KL 4"]
+target_names = ["KL 0", "KL 1", "KL 2", "KL 3", "KL 4"]
 
 model = rebuild_kneenet()
 
-kl=-1
+kl = -1
 
-stop_after = 10  # Reduce the amount of samples to evaluate for quicker results. -1 for "all"
+stop_after = -1  # Reduce the amount of samples to evaluate for quicker results. -1 for "all"
 
 for filepath_kl in filepaths:
     count = 0
-    kl+=1
+    kl += 1
+    print("current KL",kl)
     for filename in os.listdir(filepath_kl):
         if stop_after != -1 and stop_after <= count:
             break
         img = cv2.imread(os.path.join(filepath_kl, filename), 0).astype("float")
         processed_image = preprocess(img)
         logits, probabilities = predict(processed_image)
-        predictions.append(np.argmax(probabilities))
+        prediction = np.argmax(probabilities)
+
+        def overrule(prediction, probabilities):
+            """
+            Overrule prediction for KL=0 and KL=2 to KL=1 if conditions are met
+            Args:
+                prediction: int
+                probabilities: np.array
+
+            Returns: prediction
+
+            """
+            is_kl2 = prediction == 2
+            kl1_falls_between = probabilities[0] > probabilities[1] and probabilities[2] > probabilities[1]
+            not_confident = probabilities[prediction] < 0.5
+            if is_kl2 and kl1_falls_between and not_confident:
+                prediction = 1
+                print("overruled to KL=1")
+            return prediction
+
+        prediction = overrule(prediction, probabilities)
+
+        predictions.append(prediction)
         actuals.append(kl)
-        count+=1
+        count += 1
 
 report = classification_report(actuals, predictions, target_names=target_names)
 
 stop_after_str = 'all' if stop_after == -1 else str(stop_after)
-filename = 'testresults_' + stop_after_str + '.txt'
-with open(filename,'w') as file:
+filename = 'testresults_mod_kl1_' + stop_after_str + '.txt'
+with open(filename, 'w') as file:
     file.write(report)
